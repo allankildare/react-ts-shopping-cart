df8047d411723d20026f2e7c9cb1b1936a0ae70b
allankildare <allankildare27@gmail.com>
2021-10-07 18:17:06 -0300

    feat: precos formatados em real brasileiro

--- src/Item/Item.tsx
+++ src/Item/Item.tsx
@@ -1,7 +1,6 @@
 import Button from '@material-ui/core/Button'
 
 // Types
-
 import { CartItemType } from '../App'
 
 // Styles
@@ -12,16 +11,17 @@ type Props = {
     handleAddToCart: (clickedItem: CartItemType) => void
 }
 
-const Item: React.FC<Props> = ({ item, handleAddToCart }) => (
-    <Wrapper>
+
+const Item: React.FC<Props> = ({ item, handleAddToCart }) => {
+    return (<Wrapper>
         <img src={item.image} alt={item.title} />
         <div>
             <h3>{item.title}</h3>
             <p>{item.description}</p>
-            <h3>{item.price}</h3>
+            <h3>{(item.price).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</h3>
         </div>
         <Button onClick={() => handleAddToCart(item)}>Adicionar ao carrinho</Button>
-    </Wrapper>
-)
+    </Wrapper>)
+}
 
 export default Item
